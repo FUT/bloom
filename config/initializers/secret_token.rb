399a3af52612c890c865b4067ea1b8c64036b550
Yury Kaliada <fut.wrk@gmail.com>
2013-01-19 21:04:51 +0300

    Initial commit
    Signed-off-by: Yury Kaliada <fut.wrk@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloom::Application.config.secret_token = 'e877cafb59fa3da698edb2680a0c426c52f5a57653a7c5b2b4ab2f6cbd8db345b576a2c8a81b02713def81d84bb7504509e6d8ee26f25f175254fcab03bd1b53'
